f547d384a361b07f65415b62400d5c8ff32fad8f
wszx3394 <47415641+wszx3394@users.noreply.github.com>
2024-05-28 20:33:45 +0800

    修改customize.sh为自己的

--- customize.sh
+++ customize.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 #===============================================
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/config_generate
+# 修改默认IP
+#sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.15.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
+# 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
+# 修改主机名
 #sed -i 's/OpenWrt/kenzo/g' package/base-files/files/bin/config_generate
 
-#2. Custom settings
+#2. 自定义设置
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
+git clone  https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+#设置内核版本为5.10
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
+#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
+
+#通常这两个就够了
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
