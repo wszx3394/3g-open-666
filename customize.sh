2ac09b42aeb6f3ce5c4270ea30717345ceab8121
wszx3394 <47415641+wszx3394@users.noreply.github.com>
2024-09-24 02:30:34 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 #===============================================
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.1.251/g' openwrt/package/base-files/files/bin/config_generate
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# Modify default theme
+# 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
+# 修改主机名
 #sed -i 's/OpenWrt/kenzo/g' package/base-files/files/bin/config_generate
 
-#2. Custom settings
+#2. 自定义设置
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+#添加自定义插件库
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
+#设置内核版本为5.10
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
+
+#对于强迫症的同学（有报错信息、或Lean源码编译出错的情况），请尝试删除冲突的插件
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
